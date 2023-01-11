09104ac38ed540fa4a6e1cf93bdfbfb8eddee361
Hero <icraft2170@gmail.com>
2023-01-11 23:42:36 +0900

    Issue 등록 API 생성

---
+++ issue-service/src/main/kotlin/me/issueservice/model/IssueRequest.kt
@@ -0,0 +1,49 @@
+package me.issueservice.model
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import me.issueservice.domain.Issue
+import me.issueservice.domain.enums.IssuePriority
+import me.issueservice.domain.enums.IssueStatus
+import me.issueservice.domain.enums.IssueType
+import java.time.LocalDateTime
+
+data class IssueRequest(
+    val summary: String,
+    val description: String,
+    val type: IssueType,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+)
+data class IssueResponse (
+    val id: Long,
+    val summary: String,
+    val description: String,
+    val type: IssueType,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdAt: LocalDateTime?,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val updatedAt: LocalDateTime?,
+) {
+
+
+    companion object {
+        operator fun invoke(issue: Issue) =
+            with(issue) {
+                IssueResponse(
+                    id = issue.id!!,
+                    summary = issue.summary,
+                    description = issue.description,
+                    type = issue.type,
+                    priority = issue.priority,
+                    status = issue.status,
+                    createdAt = issue.createdAt,
+                    updatedAt = issue.updatedAt,
+                )
+            }
+
+
+    }
+
+}
---
+++ issue-service/src/main/kotlin/me/issueservice/service/IssueService.kt
@@ -0,0 +1,31 @@
+package me.issueservice.service
+
+import me.issueservice.domain.Issue
+import me.issueservice.domain.IssueRepository
+import me.issueservice.model.IssueRequest
+import me.issueservice.model.IssueResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class IssueService (
+    private val issueRepository: IssueRepository,
+) {
+    @Transactional
+    fun create(
+        request: IssueRequest,
+        userId: Long,
+    ): IssueResponse {
+        val issue = Issue(
+            summary = request.summary,
+            description = request.description,
+            userId = userId,
+            type = request.type,
+            priority = request.priority,
+            status = request.status,
+        )
+
+        return IssueResponse(issueRepository.save(issue))
+    }
+
+}
