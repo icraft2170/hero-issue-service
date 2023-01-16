c2692d1c70202467c6b4342bbc2991b730a5c498
Hero <icraft2170@gmail.com>
2023-01-16 13:01:38 +0900

    User Name 을 조회하는 API 생성

--- user-service/src/main/kotlin/me/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/me/userservice/controller/UserController.kt
@@ -5,6 +5,7 @@ import me.userservice.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -41,4 +42,12 @@ class UserController(
     ): MeResponse {
         return MeResponse(userService.getByToken(token))
     }
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUserName(
+        @PathVariable userId: Long,
+    ) : Map<String, String>  {
+        return mapOf("reporter" to userService.get(userId).username)
+    }
+
 }
